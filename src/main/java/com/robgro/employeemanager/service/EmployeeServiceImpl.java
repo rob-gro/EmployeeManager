f3145081a4e5dfb202cb8eaf7f7749353357b54d
RobGro <r.grodzki74@gmail.com>
2022-02-09 19:36:15 +0000

    added employee service

---
+++ src/main/java/com/robgro/employeemanager/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.robgro.employeemanager.service;
+
+import com.robgro.employeemanager.model.Employee;
+import com.robgro.employeemanager.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
