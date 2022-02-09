059cd4326099d7e6617d946d8c07e77151583ef9
RobGro <r.grodzki74@gmail.com>
2022-02-09 19:29:00 +0000

    added employee repository

---
+++ src/main/java/com/robgro/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.robgro.employeemanager.repository;
+
+import com.robgro.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
