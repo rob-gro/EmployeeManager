b9261d26cb356cbf81cdd1d5b3aaf879c55a132b
RobGro <r.grodzki74@gmail.com>
2022-02-09 19:46:04 +0000

    added employee controller

---
+++ src/main/java/com/robgro/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.robgro.employeemanager.controller;
+
+import com.robgro.employeemanager.service.EmployeeServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EmployeeController {
+
+    private final EmployeeServiceImpl employeeService;
+
+    public EmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @RequestMapping({"/","index", "index.html"})
+    public String viewHomePage(Model model) {
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());
+        return "index";
+    }
+}
