01287adfd8ee0562f123bfd44580c80d44896bb8
RobGro <r.grodzki74@gmail.com>
2022-02-09 19:36:26 +0000

    added employee service

---
+++ src/main/java/com/robgro/employeemanager/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.robgro.employeemanager.service;
+
+import com.robgro.employeemanager.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+}
